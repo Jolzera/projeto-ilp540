fa5b9a9061e2d358b78b915843beda696f9a2c04
Jonathan <100365770+Jolzera@users.noreply.github.com>
2022-04-14 10:29:50 -0300

    Update logado.php

--- logado.php
+++ logado.php
@@ -25,7 +25,7 @@
                         <a class="nav-link" href="#">Meus dados</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="index.html">Sair</a>
+                        <a class="nav-link" href="index.php">Sair</a>
                     </li>
                 </ul>
                 <form class="form-inline mt-2 mt-md-0">
